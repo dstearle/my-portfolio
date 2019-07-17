ff06eed4915b751e38365fbf9d26e99c4cb2d8f8
dstearle <dallas.st.earle@gmail.com>
2019-07-17 01:55:05 -0700

    Refactored WorkExperience into new Experience folder and refactored files

---
+++ src/components/portfolioBody/experience/ExperienceIndex.vue
@@ -0,0 +1,35 @@
+<template>
+    
+    <div class="container">
+
+        <h3>Work Experience</h3>
+                
+        <br>
+    
+        <ripe-metrics></ripe-metrics>
+
+    </div>
+    
+</template>
+
+<script>
+
+    import RipeMetrics from "./employment/RipeMetrics.vue";
+    
+    export default {
+
+        components: {
+            
+            ripeMetrics: RipeMetrics,
+            
+        },
+        
+    }
+
+</script>
+
+<style>
+
+    .imgClass3 {max-height: 250px; max-width: 400px;}
+
+</style>
