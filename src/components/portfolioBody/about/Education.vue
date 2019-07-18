e95f86a731653dbc830a32c7c725397640f078f2
dstearle <dallas.st.earle@gmail.com>
2019-07-18 02:06:54 -0700

    Refactored About into its own folder and index and Education as a component

---
+++ src/components/portfolioBody/about/Education.vue
@@ -0,0 +1,62 @@
+<template>
+    
+    <div class="container">
+
+        <h3>Education</h3>
+        
+        <br>
+        
+        <div class="row">
+            
+            <div class="col-6">
+                
+                <div class="text-align-center">
+                
+                    <img src="./../../../assets/images/palomarSeal.gif" class="imgClass3">
+                
+                </div>
+
+                <div >
+
+                    <h6>Palomar Community College (2017-2019)</h6>
+
+                    <ul>
+                        <li>Associates: Computer Science Information Technology</li>
+                        <li>Certificate: Web Development</li>
+                    </ul>
+
+                </div>
+                
+            </div>
+    
+            <div class="col-6">
+                
+                <div class="text-align-center">
+                
+                    <img src="./../../../assets/images/csusbSeal.png" class="imgClass3">
+                
+                </div>
+
+                <div >
+
+                    <h6>California State University San Bernardino (2009-2013)</h6>
+    
+                    <ul>
+                        <li>Bachelors: Anthropology</li>
+                        <li>Minor: Criminal Justice</li>
+                    </ul>
+
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <p>
+            For the past couple years I have been taking classes through Palomar College to see what the world of web development and tech was like.
+            I first started with a certificate program for web development and learned many of the basic skills needed to code such as basic html, css and javascript. When I had finished, I felt that this was definitely a career I could see myself thriving in so I decided to continue my education by completing an associates degree. From there I delved further into more advance classes and refined my skills in coding. Although I may have completed both a cerficate program and an associates, there is always more to learn!
+        </p>
+
+    </div>
+
+</template>
