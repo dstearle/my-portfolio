b4331fa53ca4845ef08842aba365cdd2db1019e1
dstearle <dallas.st.earle@gmail.com>
2019-07-23 18:55:23 -0700

    Added table for SkillsTools

--- src/components/portfolioBody/about/SkillsTools.vue
+++ src/components/portfolioBody/about/SkillsTools.vue
@@ -6,8 +6,16 @@
 
             <div class="card-body">
 
-                SkillsTools
-                
+                <table class="table table-bordered">
+                    
+                    <tr v-for="skills of mySkills">
+                        <td class="text-center">{{ skills.col1 }}</td>
+                        <td class="text-center">{{ skills.col2 }}</td>
+                        <td class="text-center">{{ skills.col3 }}</td>
+                    </tr>
+
+                </table>
+
             </div>
 
         </div>
@@ -15,3 +23,19 @@
     </div>
 
 </template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                mySkills: [
+                    { col1: "HTML5", col2: "CSS", col3: "JAVASCRIPT"},
+                    { col1: "VUE.JS", col2: "BOOTSTRAP", col3: "LARAVEL"},
+                    { col1: "PHP", col2: "SQL", col3: "MYSQL"},
+                    { col1: "C#", col2: "XML", col3: ".NET"},
+                ]
+            }
+        }
+    }
+</script>
