e153b7bbb8c069009e5e501337903f237d361037
dstearle <dallas.st.earle@gmail.com>
2019-07-26 02:01:14 -0700

    Began work on form validation for Contact

--- src/components/portfolioBody/contact/ContactForm.vue
+++ src/components/portfolioBody/contact/ContactForm.vue
@@ -12,17 +12,36 @@
 
                         <!-- Name Input -->
                         <div class="form-group">
-                            <input type="text" id="name" placeholder="Enter Name" class="form-control form-control-lg">
+                            <input 
+                                type="text" 
+                                id="name" 
+                                placeholder="Enter Name" 
+                                class="form-control form-control-lg"
+                                v-bind:class="isValid"
+                                v-model="user.name"
+                            >
                         </div>
 
                         <!-- Email Input -->
                         <div class="form-group">
-                            <input type="text" id="name" placeholder="Enter Email" class="form-control form-control-lg">
+                            <input 
+                                type="text" 
+                                id="email" 
+                                placeholder="Enter Email" 
+                                class="form-control form-control-lg"
+                                v-model="user.email"
+                            >
                         </div>
 
                         <!-- Text Area -->
                         <div class="form-group">
-                            <textarea id="message" placeholder="Interested in contacting me? Write me a message here..." rows="3" class="form-control"></textarea>
+                            <textarea 
+                                id="message" 
+                                placeholder="Interested in contacting me? Write me a message here..." 
+                                rows="3" 
+                                class="form-control"
+                                v-model="user.message"
+                            ></textarea>
                         </div>
 
                         <div class="pt-3">
@@ -40,3 +59,37 @@
     </div>
 
 </template>
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+                user: {
+                    email: '',
+                    name: '',
+                    message: '',
+                    valid: false,
+                }
+            }
+        },
+
+        computed: {
+            isValid: function() {
+                
+                return this.user.valid ? 'is-valid' : 'is-invalid'
+                return console.log(this.user.valid) 
+                
+            }
+        },
+
+        methods: {
+            handleSubmit() {
+            // Send data to the server or update your stores and such.
+            }
+        }
+
+    }
+
+</script>
