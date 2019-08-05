1f1c07c55242575558d2fe402b6f5745315877d7
dstearle <dallas.st.earle@gmail.com>
2019-08-05 02:16:32 -0700

    Created file for VueStockTrader

---
+++ src/components/portfolioBody/experience/projects/VueStockTrader.vue
@@ -0,0 +1,42 @@
+<template>
+    
+    <div class="card project-card">
+
+        <!-- Project Image -->
+        <img src="./../../../../assets/images/laravelQaImg.png" class="card-img-top" alt="Laravel Project">
+
+        <div class="card-body">
+
+            <!-- Project Title -->
+            <h5 class="card-title">Laravel-Qa</h5>
+
+            <!-- Project Description -->
+            <p class="card-text">
+                I made this project using Laravel and PHP to demonstrate the concept of the MVC (Model View Controller).
+                It is a basic question and answer forum where users can login, ask questions and give answers to questions.
+                Users can also upvote questions and answers or favorite them for reference.
+            </p>
+
+            <!-- Card Footer -->
+            <div class="row" style="padding:0px 20px 0px 20px">
+
+                <strong class="ml-auto">
+
+                    <!-- Github Repo -->
+                    <a href="https://github.com/dstearle/laravel-qa">github repo</a>
+
+                </strong>
+
+            </div>
+            
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+    .project-card {min-height: 280px;}
+    
+</style>
