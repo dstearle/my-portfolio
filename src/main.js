f8857b190ae943a284f971b626821856974f6f7b
dstearle <dallas.st.earle@gmail.com>
2019-08-25 09:46:28 -0700

    Added vue-resource

--- src/main.js
+++ src/main.js
@@ -5,14 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import jQuery from 'jquery';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import Vuelidate from 'vuelidate';
 import VueClipboard from 'vue-clipboard2'
 
 import { routes } from './routes';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 Vue.use(Vuelidate);
 Vue.use(VueClipboard);
+
+// VueResource link to firebase database
+Vue.http.options.root = 'https://vuejs2-27f10.firebaseio.com/';
 
 Vue.config.productionTip = false
 
